cf348fc2515a3c67adffd9a423b3dc79cecb6ef7
mashutiks <mashutiks@icloud.com>
2021-05-28 13:58:52 +0300

    port changed from 8000 to 80

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use('/', express.static(path.join(__dirname, '../static')));
-const port = 8000;
+const port = 80;
 app.listen(port, "0.0.0.0", function() {
     console.log(`App listening at port ${port}`);
 })
